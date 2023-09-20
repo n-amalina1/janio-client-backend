2a7138b5901b672e96274263d44d57a509308af4
n-amalina1 <milesawaaay@gmail.com>
2023-09-20 18:36:54 +0800

    Added cors and post to admin

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 
 	routes "janio-client-backend/routes"
 )
@@ -10,4 +14,15 @@ var db *sql.DB
 
 func main() {
 	routes.SetupRoutes(db)
+
+	resp, err := http.Get("http://localhost:8080/client/orders")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	sb := string(body)
+	fmt.Printf(sb)
 }
--- routes/route.go
+++ routes/route.go
@@ -14,9 +14,27 @@ var db *sql.DB
 func SetupRoutes(d *sql.DB) {
 	db = d
 	router := gin.Default()
+	router.Use(CORSMiddleware())
+
 	router.GET("new/orders", GetOrdersClient)
 
 	router.Run("localhost:8008")
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	}
 }
 
 func GetOrdersClient(c *gin.Context) {
