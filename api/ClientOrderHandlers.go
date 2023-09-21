b54b2956279b3c5687682fdb5e837d7de4f9b1d6
n-amalina1 <milesawaaay@gmail.com>
2023-09-21 20:40:40 +0800

    Added interval to pushing to admin

--- api/ClientOrderHandlers.go
+++ api/ClientOrderHandlers.go
@@ -1,7 +1,14 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
 	"janio-client-backend/models"
+	"log"
+	"net/http"
+	"time"
 )
 
 func FormatClientToDB(orders []models.ClientOrder) []models.ClientToDBOrder {
@@ -22,4 +29,47 @@ func FormatClientToDB(orders []models.ClientOrder) []models.ClientToDBOrder {
 		ordersC = append(ordersC, order)
 	}
 	return ordersC
+}
+
+func pushToAdmin() {
+	bodyBytes, err := json.Marshal(models.OrdersDB)
+	if err != nil {
+		log.Fatal(err)
+	}
+	reader := bytes.NewReader(bodyBytes)
+
+	resp, err := http.Post("http://localhost:8080/client/orders", "application/json", reader)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	sb := string(body)
+	fmt.Printf("%s\n", sb)
+}
+
+func PushToAdminInterval() {
+	ticker := time.NewTicker(2 * time.Second)
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+
+				pushToAdmin()
+			case <-done:
+
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+
+	time.Sleep(10 * time.Second)
+	done <- true
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,44 +1,14 @@
 package main
 
 import (
-	"bytes"
 	"database/sql"
-	"encoding/json"
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-
-	"janio-client-backend/models"
+	"janio-client-backend/api"
 	"janio-client-backend/routes"
 )
 
 var db *sql.DB
 
 func main() {
-	fmt.Println("Welcome to main() function")
-	someFunction()
-
+	api.PushToAdminInterval()
 	routes.SetupRoutes(db)
-}
-
-func someFunction() {
-	bodyBytes, err := json.Marshal(models.OrdersDB)
-	if err != nil {
-		log.Fatal(err)
-	}
-	reader := bytes.NewReader(bodyBytes)
-
-	resp, err := http.Post("http://localhost:8080/client/orders", "application/json", reader)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	sb := string(body)
-	fmt.Printf("%s\n", sb)
 }
